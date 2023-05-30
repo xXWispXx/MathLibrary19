04062d4077c1498fd50945dfa37be8adc08a4bbe
xXWispXx <125870013+xXWispXx@users.noreply.github.com>
2023-05-30 16:24:18 +0300

    1

---
+++ LibLesson19/MathLibrary19/Trigonometry.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathLibrary19
+{
+    public static class Trigonometry
+    {
+        public static double Sin(double BC, double AB)
+        {
+            return BC / AB;
+        }
+
+        public static double Cos(double AC, double AB)
+        {
+            return AC / AB;
+        }
+
+        public static double Tg(double BC, double AC)
+        {
+            return BC / AC;
+        }
+
+        public static double Ctg(double BC, double AC)
+        {
+            return 1 / (BC / AC);
+        }
+
+        public static double ArcSin(double BC, double AC)
+        {
+            return 1 / (BC / AC);
+        }
+    }
+}
